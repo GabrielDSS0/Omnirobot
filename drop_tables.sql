1acb77fc748f53023e74b73da1b1942caaccc63f
Kalest3 <gatchmew@gmail.com>
2024-10-12 17:12:28 -0300

    this line isn't supposed to be here

--- drop_tables.sql
+++ drop_tables.sql
@@ -3,5 +3,4 @@ drop table omni_schema.tbl_leaderboard
 drop table omni_schema.tbl_user
 drop table omni_schema.tbl_room
 drop table omni_schema.tbl_dp_action
-drop table omni_schema.tbl_dp_move
 drop table omni_schema.tbl_dp_game
