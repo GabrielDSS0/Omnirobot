40eaba86f7557aaeff27fafeea2734d50318aa36
Kalest3 <gatchmew@gmail.com>
2024-10-13 19:23:28 -0300

    update in gitpull

--- src/admin_commands/commands.py
+++ src/admin_commands/commands.py
@@ -1,6 +1,6 @@
+import importlib
+import sys
 import subprocess
-
-from websockets.exceptions import ConnectionClosed
 
 from src.vars import Varlist
 from src.sending import respondPM
@@ -14,10 +14,28 @@ class Admin_Commands():
         func()
 
     def kill(self):
-        raise ConnectionClosed(None, None)
+        sys.exit()
     
     def gitpull(self):
         command = ["git", "pull"]
-        output = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
-        results = (output.stdout + output.stderr).decode('utf-8')
-        respondPM(self.senderID, f"!code {results}")
+        output_git = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+        results_git = (output_git.stdout + output_git.stderr).decode('utf-8').strip()
+
+        respondPM(self.senderID, f"!code {results_git}")
+
+        if results_git == "Already up to date.":
+            return
+
+        results_git = results_git.split("\n")
+        
+        for result in results_git:
+            if result.startswith(" "):
+                pipes = result.count("|")
+                if not pipes:
+                    continue
+                result = result.split("|", 1)[0].strip()
+                result = result.replace("/", ".")
+                result = result.replace(".py", "")
+
+                module = __import__(result)
+                importlib.reload(module)
