f126d88ff9f2a4567d7ac30593bbba3fd005c84e
Kalest3 <gatchmew@gmail.com>
2024-10-16 16:14:00 -0300

    create the pkl file if not exists

--- objectsdill.py
+++ objectsdill.py
@@ -12,6 +12,9 @@ def save():
         dill.dump(vars.Varlist.hosts_groupchats, f)
 
 def load():
+    if not (os.path.exists(pkl_file)):
+        open(pkl_file, "x")
+
     with open(pkl_file, 'rb') as f:
         if not (os.stat(pkl_file).st_size == 0) and not (vars.Varlist.dpGames or vars.Varlist.hosts_groupchats):
             vars.Varlist.dpGames = dill.load(f)
